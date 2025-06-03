d0eddec6540fae308262be634dfee3a5a9daa7dc
Igor Guimarães <igorgpstm@gmail.com>
2025-06-03 15:54:58 -0300

    feat: criei o component header e setei o layout basico

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header from './components/Header'
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <main className="flex flex-col items-center justify-center min-h-screen">
+        <h2 className="text-4xl text-pink-600 font-bold">Bem-vindo ao My Love Gift 🎁</h2>
+      </main>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header className="w-full p-4 bg-pink-300 shadow-lg">
+      <h1 className="text-3xl font-bold text-white">My Love Gift 💖</h1>
+    </header>
+  )
+}
